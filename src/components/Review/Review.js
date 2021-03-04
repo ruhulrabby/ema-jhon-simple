6ba309412fb0d1bec6545771087933830eacf9d6
ruhul rabby <ruhulalam1994@gmail.com>
2021-03-04 20:38:52 +0600

    done

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import fakeData from '../../fakeData';
+import { getDatabaseCart } from '../../utilities/databaseManager';
+import ReviewAllItem from '../ReviewAllItem/ReviewAllItem';
 
 const Review = () => {
+
+    const [cart, setCart] = useState([]);
+
+    useEffect(() =>{
+        //cart
+        const savedCart = getDatabaseCart();
+        const productKeys = Object.keys(savedCart);
+        const cartProducts =  productKeys.map(key => {
+            const product = fakeData.find(pd => pd.key === key);
+            product.quantity = savedCart[key];
+            return product;
+        });
+        setCart(cartProducts);
+    }, [])
     return (
         <div>
-            <h2>This is Review</h2>
+            <h2>Cart Items: {cart.length}</h2>
+            {
+                cart.map(pd => <ReviewAllItem product= {pd} ></ReviewAllItem>)
+            }
+            
+            
+            
         </div>
     );
 };
 
 export default Review;
+
+// 04.28 to start again
---
+++ src/components/ReviewAllItem/ReviewAllItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ReviewAllItem = () => {
+    return (
+        <div>
+            <h2>this is to reivew</h2>
+        </div>
+    );
+};
+
+export default ReviewAllItem;
